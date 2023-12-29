128f9f4a81e9c99cb169d9df1981014a18f61fff
naushad.shukoor <naushad.shukoor@ril.com>
2023-12-29 11:37:08 +0530

    add files

---
+++ edtech/app.py
@@ -0,0 +1,77 @@
+from pathlib import Path
+import streamlit as st
+from langchain.vectorstores import FAISS
+from langchain.llms import GooglePalm
+from langchain.document_loaders.csv_loader import CSVLoader
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+
+from dotenv import load_dotenv
+load_dotenv()
+
+# Create Google Palm LLM model
+llm = GooglePalm(temperature=0.1)
+# # Initialize instructor embeddings using the Hugging Face model
+instructor_embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-large")
+vectordb_file_path = "faiss_index"
+
+def create_vector_db():
+    # Load data from FAQ sheet
+    loader = CSVLoader(file_path='codebasics_faqs.csv', source_column="prompt")
+    data = loader.load()
+
+    print("Starting indexing process...")
+    # Create a FAISS instance for vector database from 'data'
+    vectordb = FAISS.from_documents(documents=data,
+                                    embedding=instructor_embeddings)
+
+    # Save vector database locally
+    vectordb.save_local(vectordb_file_path)
+    print("Finished indexing process...")
+
+
+def get_qa_chain():
+    # Load the vector database from the local folder
+    vectordb = FAISS.load_local(vectordb_file_path, instructor_embeddings)
+
+    # Create a retriever for querying the vector database
+    retriever = vectordb.as_retriever(score_threshold=0.7)
+
+    prompt_template = """Given the following context and a question, generate an answer based on this context only.
+    In the answer try to provide as much text as possible from "response" section in the source document context without making much changes.
+    If the answer is not found in the context, kindly state "I don't know." Don't try to make up an answer.
+
+    CONTEXT: {context}
+
+    QUESTION: {question}"""
+
+    PROMPT = PromptTemplate(
+        template=prompt_template, input_variables=["context", "question"]
+    )
+
+    chain = RetrievalQA.from_chain_type(llm=llm,
+                                        chain_type="stuff",
+                                        retriever=retriever,
+                                        input_key="query",
+                                        return_source_documents=True,
+                                        chain_type_kwargs={"prompt": PROMPT})
+    return chain
+faiss_index_exists = (Path.cwd() / 'faiss_index').exists()
+
+st.title("Codebasics Q&A 🌱")
+
+if faiss_index_exists:
+    st.write("Knowledgebase is already created!!! You can ask questions below.")
+btn = st.button("Create Knowledgebase")
+if btn:
+    create_vector_db()
+    st.write("Knowledgebase is created!!! You can ask questions now.")
+question = st.text_input("Question: ")
+
+if question:
+    chain = get_qa_chain()
+    response = chain(question)
+
+    st.header("Answer")
+    st.write(response["result"])
